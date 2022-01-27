c5c4c764cfcd5ce0e8639249fadbc9532c767126
Przemyslaw Kleszcz <kleszcz@mail.com>
2022-01-27 02:42:31 +0100

    Updates

---
+++ precache-manifest.8a8c3919f9af42c8c63bb11e6fa55f1e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3645b4fe540daa9adbf3a85b1d93e8f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7eb0aa4d6b5fcb59d1b",
+    "url": "/static/css/2.704d6fdf.chunk.css"
+  },
+  {
+    "revision": "f0c4ec28f475dae71db7",
+    "url": "/static/css/main.cb9b3f93.chunk.css"
+  },
+  {
+    "revision": "e7eb0aa4d6b5fcb59d1b",
+    "url": "/static/js/2.9a0fefbb.chunk.js"
+  },
+  {
+    "revision": "f0c4ec28f475dae71db7",
+    "url": "/static/js/main.78143212.chunk.js"
+  },
+  {
+    "revision": "5ab5da710d97acabba7a",
+    "url": "/static/js/runtime~main.708f7b04.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
